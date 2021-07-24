68785af558fc362e5c21ca7e0e4ca074b4db3e53
PatrickTDewey <ptddev@protonmail.com>
2021-07-24 11:24:48 -0700

    refactored likes to use one js function

--- likes/js/main.js
+++ likes/js/main.js
@@ -1,16 +1,17 @@
 let likes = [0,0,0];
 
-function countLike1() {
-    likes[0]++;
-    document.querySelector("#col-likes").innerHTML = `${likes[0]} Like(s)`;
-}
-
-function countLike2() {
-    likes[1]++;
-    document.querySelector("#col2-likes").innerHTML = `${likes[1]} Like(s)`;
-}
-
-function countLike3() {
-    likes[2]++;
-    document.querySelector("#col3-likes").innerHTML = `${likes[2]} Like(s)`;
+function countLikes(e) {
+    switch(e.id){
+        case 'button1':
+            likes[0]++;
+            document.querySelector("#col-likes").innerHTML = `${likes[0]} Like(s)`;
+            break;
+        case 'button2':
+            likes[1]++;
+            document.querySelector("#col2-likes").innerHTML = `${likes[1]} Like(s)`;
+            break;
+        case 'button3':
+            likes[2]++;
+            document.querySelector("#col3-likes").innerHTML = `${likes[2]} Like(s)`;
+    }
 }
