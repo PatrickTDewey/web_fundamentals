e0021ce621152439ae18ce2d7b9b5b88005618c7
PatrickTDewey <ptddev@protonmail.com>
2021-07-25 23:09:10 -0700

    added weather dojo assignment

---
+++ dojo_weather/main.js
@@ -0,0 +1,26 @@
+function loadingReport() {
+    alert('Loading weather report...');
+}
+
+let cookie = document.querySelector('.cookie-policy');
+function hide() {
+    cookie.remove();
+}
+
+let degreeArr = document.getElementsByClassName('degree');
+function convertTemp(e) {
+    switch (e.value) {
+        case 'celsius':
+            for (let index = 0; index < degreeArr.length; index++) {
+                let temp = ((Number(degreeArr[index].innerHTML) - 32) * (5 / 9)) ;
+                degreeArr[index].innerHTML = Math.floor(temp);
+            }
+            break;
+        case 'farenheit':
+            for (let index = 0; index < degreeArr.length; index++) {
+                let temp = (Number(degreeArr[index].innerHTML) * ( 9 / 5 )) + 32;
+                degreeArr[index].innerHTML = Math.floor(temp);
+            }
+            break;
+    }
+}
--- lessons/javascript/js/main.js
+++ lessons/javascript/js/main.js
@@ -22,3 +22,24 @@ function over(e) {
 function out(e) {
     alert('mouseout')
 }
+
+// function asyncFunc(){
+//     for (let index = 0; index < 1000; index++) {
+//         console.log(`running ${index}`);
+        
+//     }
+// }
+
+// setTimeout(asyncFunc, 10000);
+function setName(e) {
+    console.log(e.value);
+    document.getElementById('name').innerHTML = e.value
+}
+
+function pickFood(e) {
+    console.log(`The food is ${e.value}`);
+}
+
+function orderFood() {
+    alert('Ordering a ' + document.querySelector('#food-select').value);
+}
