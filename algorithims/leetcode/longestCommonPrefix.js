6abd8c6f7597d3273f445f5f627168f77cea9e03
PatrickTDewey <ptddev@protonmail.com>
2021-08-08 10:34:20 -0700

    added lcp algo

--- algorithims/leetcode/longestCommonPrefix.js
+++ algorithims/leetcode/longestCommonPrefix.js
@@ -2,10 +2,18 @@ let testOne = ["flower", "flood", "flight"];
 let testTwo = ["dog", "cat", "racecar"];
 
 var longestCommonPrefix = function(strs) {
-    for (let index = 0; index < strs.length; index++) {
-        console.log(strs[index]);
-        
+    // set prefix to first string in index
+    let prefix = strs[0];
+
+    // loop through other strings in index
+    for(let i = 1; i < strs.length; i++){
+        while (strs[i].indexOf(prefix) != 0) {
+            prefix = prefix.substring(0, prefix.length -1)
+            if (prefix === '') {
+                return '';
+            }
+        }
     }
-    return 'lol';
+    return prefix;
 };
 console.log(longestCommonPrefix(testOne));
