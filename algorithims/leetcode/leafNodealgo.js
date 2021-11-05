e36cc83ddf3c4d40d73243adcf2568bb73eefbc8
PatrickTDewey <ptddev@protonmail.com>
2021-11-05 10:28:14 -0700

    added arrange coins

---
+++ algorithims/leetcode/leafNodealgo.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var sumNumbers = function(root) {
+    let sum = 0;
+    checkNodes(root, 0, sum)
+    return sum;
+    
+};
+
+// recursive func
+
+var checkNodes = function(root, num, sum){
+    if (node === null){
+        return
+    }
+    const currentNumber = num * 10 + root;
+    if (root.left === null && root.right === null) {
+        sum += currentNumber
+    }
+    checkNodes(node.right, currentNumber, sum)
+    checkNodes(node.left, currentNumber, sum)
+}
