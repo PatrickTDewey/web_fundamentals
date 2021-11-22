a42004a1e09b8a0d1f7abc92ecd93e7754aa4ac0
PatrickTDewey <ptddev@protonmail.com>
2021-11-22 09:47:02 -0800

    added daily algo

---
+++ algorithims/leetcode/deleteNodeInBST.js
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+var deleteNode = function(root, key) {
+    // if root is empty
+    if(root == null){
+        return null;
+    }
+    
+    
+    if(key < root.val){
+        root.left = deleteNode(root.left, key);
+    }
+    else if (key > root.val){
+        root.right = deleteNode(root.right, key);
+    } else {
+        // if no children return null
+        if(root.left == null && root.right == null){
+            return null;
+        }
+        // if one child return the child
+        else if(root.left == null){
+            return root.right;
+        } else if (root.right == null){
+            return root.left;
+        }
+        
+        // if two children
+        // set roots val to the min of the right
+        root.val = minVal(root.right);
+        // delete the min val you found down the right tree
+        root.right = deleteNode(root.right, root.val)
+    }
+    return root;
+}
+const minVal = (root) => {
+    let min = root.val;
+    
+    while(root.left != null){
+        min = root.left.val;
+        root = root.left;
+    }
+    return min;
+}
