f1cf71f633cd26ec265d544810068c6967f81966
PatrickTDewey <ptddev@protonmail.com>
2022-05-27 13:54:14 -0700

    refactor: shortened solution to reduce to zero

--- algorithims/leetcode/java_algos/ReduceToZero.java
+++ algorithims/leetcode/java_algos/ReduceToZero.java
@@ -1,21 +1,8 @@
 package java_algos;
 
 public class ReduceToZero {
-	private int steps;
-
-	public ReduceToZero() {
-	}
-
 	public int numberOfSteps(int num) {
-		if (num == 0)
-			return steps;
-
-		steps++;
-
-		if (num % 2 == 0) {
-			return numberOfSteps(num / 2);
-		} else {
-			return numberOfSteps(num - 1);
-		}
+		if (num == 0) return 0;
+		return num % 2 == 0 ? 1 + numberOfSteps(num / 2) : 1 + numberOfSteps(num - 1);
 	}
 }
